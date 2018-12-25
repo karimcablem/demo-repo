a6b178a57b4a9f24bdaa39594121ab9dfb48f6c8
kom <kom@izzi.mx>
2018-12-25 14:33:47 -0600

    Commit inicial

---
+++ src/main/java/my/vaadin/app/CustomerFormDesign.java
@@ -0,0 +1,68 @@
+package my.vaadin.app;
+
+import com.vaadin.data.Binder;
+import com.vaadin.event.ShortcutAction.KeyCode;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.DateField;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.NativeSelect;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.themes.ValoTheme;
+
+public class CustomerFormDesign extends FormLayout {
+	private TextField firstName= new TextField("First Name");
+	private TextField lastName= new TextField("Last Name");
+	private TextField email= new TextField("Email");
+	private NativeSelect<CustomerStatus> status= new NativeSelect<>("Status");
+	private DateField birthDate= new DateField("Birthday");
+	
+	private Button save= new Button("Save");
+	private Button delete= new Button("Delete");
+	
+	private CustomerService service= CustomerService.getInstance();
+	private Customer customer;
+	
+	private MyUI myUI;
+	private Binder<Customer> binder= new Binder<>(Customer.class);
+	
+	public CustomerFormDesign(MyUI myUI) {
+		this.myUI= myUI;
+		
+		setSizeUndefined();
+		HorizontalLayout buttons= new HorizontalLayout(save, delete);
+		addComponents(firstName, lastName, email, status, birthDate, buttons );
+		
+		
+		status.setItems(CustomerStatus.values());
+		save.setStyleName(ValoTheme.BUTTON_PRIMARY);
+		save.setClickShortcut(KeyCode.ENTER);
+		binder.bindInstanceFields(this);
+		
+		save.addClickListener(e-> save());
+		delete.addClickListener(e-> delete());
+		
+	}
+	
+	public void setCustomer(Customer customer) {
+		this.customer= customer;
+		binder.setBean(customer);
+		delete.setVisible(customer.isPersisted());
+		
+		setVisible(true);
+		firstName.selectAll();
+		
+	}
+	
+	public void delete() {
+		service.delete(customer);
+		myUI.updateList();
+		setVisible(false);
+	}
+	
+	public void save() {
+		service.save(customer);
+		myUI.updateList();
+		setVisible(false);
+	}
+}
